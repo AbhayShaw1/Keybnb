4f9b34373007c8c6b26ebae389184c02982f3d13
AbhayShaw1 <abhayshaw01ab@gmail.com>
2025-07-28 14:28:31 +0530

    bug: added the seedlocaldatabse function call to fetch listings

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import ReactDOM from 'react-dom/client';
 
+import { seedLocalDatabase } from './api/data/seed';
 import App from './App';
 
 import './index.css';
 
+seedLocalDatabase();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
