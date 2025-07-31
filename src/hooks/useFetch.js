74679b3cf896927beac59e98543afdf7cfdaee3e
AbhayShaw1 <abhayshaw01ab@gmail.com>
2025-07-31 06:10:26 -0700

    refact: cached the api call to avoid unnecessary fetch calls

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,16 +1,32 @@
 import axios from 'axios';
-import { useEffect, useRef, useState } from 'react';
+import {useEffect, useMemo, useRef, useState} from 'react';
 
 import api from '@/api';
+import {getItem, setItem} from "@/lib/utils/localStorage.js";
 
+const STALE_TIME = 5 * 6 * 1000;
 const useFetch = (url, options) => {
   const [data, setData] = useState();
   const [isLoading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   const abortController = useRef(null);
+  const storageKey = useMemo(()=>{
+    if(!options?.params) return url;
+    return url+'?'+JSON.stringify(options.params);
+  },[options,url]);
+
+
   useEffect(() => {
     const fetchingList = async () => {
+      const currentTime = new Date().getTime();
+      const cachedData = getItem(storageKey);
+      if(cachedData && currentTime - cachedData.lastFetched < STALE_TIME) {
+        setData(cachedData.data);
+        setLoading(false);
+        return;
+
+      }
       setLoading(true);
       setError(null);
       abortController.current = new AbortController();
@@ -33,7 +49,15 @@ const useFetch = (url, options) => {
     return () => {
       abortController.current?.abort();
     };
-  }, [options, url]);
+  }, [options, url,storageKey]);
+
+  useEffect(() => {
+    if(!data) return;
+    setItem(storageKey,{
+      lastFetched: new Date().getTime(),
+      data
+    })
+  }, [data,storageKey]);
   return { error, isLoading, data };
 };
 
