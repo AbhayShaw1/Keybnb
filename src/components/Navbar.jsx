02a071a8f3c37ac3fe4612b8fe2a9069ab64c695
AbhayShaw1 <abhayshaw01ab@gmail.com>
2025-08-02 16:48:18 +0530

    feat : added signout page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,27 @@
 import { Link } from 'react-router-dom';
 
-import { Button, Separator } from '@/components/ui';
+import api from '@/api';
+import { useAuth } from '@/components/AuthProvider.jsx';
+import {
+  Button,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  Separator,
+} from '@/components/ui';
 
 const Navbar = () => {
+  const { setToken } = useAuth();
+
+  const handleSignOut = async () => {
+    try {
+      await api.post('/auth/signOut');
+      setToken(null);
+    } catch {
+      setToken(null);
+    }
+  };
   return (
     <>
       <div className='flex flex-row justify-center gap-8 px-8 py-2'>
@@ -12,8 +31,20 @@ const Navbar = () => {
         <Button asChild variant='ghost'>
           <Link to='/favorites'>Favorites</Link>
         </Button>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Button asChild variant='ghost'>
+              <Link>Account</Link>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align='end'>
+            <DropdownMenuItem onClick={handleSignOut}>
+              Sign Out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
-      <Separator/>
+      <Separator />
     </>
   );
 };
