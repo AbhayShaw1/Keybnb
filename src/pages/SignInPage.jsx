a978879ba655a21e89f262ea960e258b49497b09
AbhayShaw1 <abhayshaw01ab@gmail.com>
2025-08-02 16:34:27 +0530

    add : logic to redirect to / on signin

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,6 +1,17 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { useAuth } from '@/components/AuthProvider.jsx';
 import SignInForm from '@/components/SignInForm.jsx';
 
 const SignInPage = () => {
+  const { token } = useAuth();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (token) {
+      navigate('/', { replace: true });
+    }
+  }, [navigate, token]);
   return (
     <div className='container flex h-screen items-center justify-center py-4'>
       <SignInForm />
